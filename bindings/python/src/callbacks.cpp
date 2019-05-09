1ca18483b4f41fa134b6b941b4a10ec01f6875cf
Avinash Madavan <madavan2@illinois.edu>
2019-05-09 17:02:41 -0500

    Add python bindings for callbacks.
    Create Python bindings for the the HDF5 saving callback. The function callback causes segmentation faults and has been excluded.

---
+++ bindings/python/src/callbacks.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Avinash Madavan on 2019-05-09.
+//
+
+#include "callbacks.h"
+
+void init_callbacks(py::module &m) {
+  py::class_<stuka::util::callback::BaseCallback,
+      std::shared_ptr<stuka::util::callback::BaseCallback>>(m, "BaseCallback");
+
+  // TODO: Fix function callback routine
+//  py::class_<stuka::util::callback::Function, stuka::util::callback::BaseCallback,
+//      std::shared_ptr<stuka::util::callback::Function>>(m, "FunctionCallback")
+//      .def(py::init<const std::function<void(stuka::OptimizeState)> &>(), py::arg("function"));
+
+  py::class_<stuka::util::callback::SaveHDF5, stuka::util::callback::BaseCallback,
+      std::shared_ptr<stuka::util::callback::SaveHDF5>>(m, "SaveHDF5Callback")
+      .def(py::init<const std::string &, const size_t>(), py::arg("filename"), py::arg("n_entries") = 0);
+}
