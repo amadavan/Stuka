954d7f0079881286ef04d53ac3330bbea8410c00
Avinash Madavan <madavan2@illinois.edu>
2019-12-14 16:50:12 -0600

    Clean up bindings code to be more legible

--- bindings/python/src/stukapy.cpp
+++ bindings/python/src/stukapy.cpp
@@ -60,9 +60,9 @@ PYBIND11_MODULE(stukapy, m) {
 #endif
       .value("BENDER", stuka::Solver::BENDER)
       .value("CRE", stuka::Solver::CRE)
-.value("MPC", stuka::Solver::MPC)
-.value("PDSS", stuka::Solver::PDSS)
-.value("PDSS2", stuka::Solver::PDSS2);
+      .value("MPC", stuka::Solver::MPC)
+      .value("PDSS", stuka::Solver::PDSS)
+      .value("PDSS2", stuka::Solver::PDSS2);
 
   py::enum_<stuka::ConstraintReductionMethods>(m, "constraintReductionMethods")
       .value("BOUNDS", stuka::ConstraintReductionMethods::BOUNDS);
@@ -78,31 +78,26 @@ PYBIND11_MODULE(stukapy, m) {
       .def_readonly("nit", &stuka::OptimizeState::nit)
       .def_readonly("nit_sub", &stuka::OptimizeState::nit_sub)
       .def_readonly("runtime", &stuka::OptimizeState::runtime)
-.def("__repr__", [](
-const stuka::OptimizeState &state
-) {
+      .def("__repr__", [](const stuka::OptimizeState &state) {
         std::stringstream ss;
         ss << state << std::endl;
         return ss.str();
       })
-.def("__getitem__", [](
-const stuka::OptimizeState &state,
-const std::string &key
-) {
-        std::variant<Eigen::VectorXd, double, size_t, int, std::string> ret = "Key not found.";;
-        if (key == "x") ret = state.x;
-        if (key == "dual_ub") ret = state.dual_ub;
-        if (key == "dual_eq") ret = state.dual_eq;
-        if (key == "fun") ret = state.fun;
-        if (key == "error") ret = state.error;
-        if (key == "status") ret = state.status;
-        if (key == "nit") ret = state.nit;
-        if (key == "nit_sub") ret = state.nit_sub;
-        if (key == "runtime") ret = state.runtime;
-        return ret;
-},
-py::is_operator()
-);
+      .def("__getitem__", [](const stuka::OptimizeState &state, const std::string &key) {
+             std::variant<Eigen::VectorXd, double, size_t, int, std::string> ret = "Key not found.";;
+             if (key == "x") ret = state.x;
+             if (key == "dual_ub") ret = state.dual_ub;
+             if (key == "dual_eq") ret = state.dual_eq;
+             if (key == "fun") ret = state.fun;
+             if (key == "error") ret = state.error;
+             if (key == "status") ret = state.status;
+             if (key == "nit") ret = state.nit;
+             if (key == "nit_sub") ret = state.nit_sub;
+             if (key == "runtime") ret = state.runtime;
+             return ret;
+           },
+           py::is_operator()
+      );
 
   py::class_<stuka::Options>(m, "Options")
       .def(py::init<>())
@@ -112,7 +107,7 @@ py::is_operator()
       .def_readwrite("step", &stuka::Options::step)
       .def_readwrite("dual_ub0", &stuka::Options::dual_ub0)
       .def_readwrite("dual_eq0", &stuka::Options::dual_eq0)
-.def_readwrite("measure", &stuka::Options::measure)
+      .def_readwrite("measure", &stuka::Options::measure)
       .def_readwrite("lp_solver", &stuka::Options::lp_solver)
       .def_readwrite("qp_solver", &stuka::Options::qp_solver)
       .def_readwrite("dlp_solver", &stuka::Options::dlp_solver)
@@ -125,29 +120,26 @@ py::is_operator()
   init_callbacks(mcallback);
 
   m.def("linprog", py::overload_cast<const stuka::LP::LinearProgram &, const stuka::Options &>(&stuka::util::linprog),
-R"pbdoc( solve a linear program )pbdoc",
-py::arg("LinearProgram"),
-py::arg("Options") =
-stuka::Options()
-);
+        R"pbdoc( solve a linear program )pbdoc",
+        py::arg("LinearProgram"),
+        py::arg("Options") = stuka::Options()
+  );
 
-m.def("quadprog", &stuka::util::quadprog, R"pbdoc( solve a quadratic program )pbdoc",
-py::arg("QuadraticProgram"),
+  m.def("quadprog", &stuka::util::quadprog, R"pbdoc( solve a quadratic program )pbdoc",
+        py::arg("QuadraticProgram"),
         py::arg("Options") = stuka::Options());
 
   m.def("linprog",
         py::overload_cast<const stuka::dLP::DecomposedLinearProgram &, const stuka::Options &>(&stuka::util::linprog),
         R"pbdoc( solve a decomposed linear program )pbdoc",
-py::arg("DecomposedLinearProgram"),
-py::arg("Options") =
-stuka::Options()
-);
+        py::arg("DecomposedLinearProgram"),
+        py::arg("Options") = stuka::Options()
+  );
 
-m.def("stochastic", &stuka::util::stochastic, R"pbdoc( solve a CVaR-stochastic convex problem )pbdoc",
-py::arg("Program"),
-py::arg("Options") =
-stuka::Options()
-);
+  m.def("stochastic", &stuka::util::stochastic, R"pbdoc( solve a CVaR-stochastic convex problem )pbdoc",
+        py::arg("Program"),
+        py::arg("Options") = stuka::Options()
+  );
 
 #ifdef VERSION_INFO
   m.attr("__version__") = VERSION_INFO;
